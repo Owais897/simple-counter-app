bdf9df618a47b1098b56c877eb923f3c5f98f21f
Owais897 <m.owais2118@gmail.com>
2019-12-26 14:37:25 +0500

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+class NavBar extends PureComponent {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
